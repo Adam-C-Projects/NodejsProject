81cb41a05feaac8351c95776cf3e2f45142c7dd7
asc50 <asc50@kent.ac.uk>
2024-11-22 20:59:37 +0000

    changed connection settings to point to adams dragon server hosted by the uni. should allow everyone access if vpn is in use

--- scripts/db_connect.php
+++ scripts/db_connect.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection settings
-$host = "Mylocalconnection";
-$user = "root";       // Replace with your MySQL username
-$password = "Gold_tiger12";       // Replace with your MySQL password
-$database = "Recipes4U";
+$host = "dragon.kent.ac.uk"; // Full server address
+$user = "asc50";             // Your MySQL username
+$password = "3ydonef";       // Your MySQL password
+$database = "asc50";         // Your database name
 
 // Create a connection
 $conn = new mysqli($host, $user, $password, $database);
@@ -11,5 +11,8 @@ $conn = new mysqli($host, $user, $password, $database);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}
+else{
+    echo "success";
 }
 ?>
