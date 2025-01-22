5f6f1cba16da8cff7506491cba88a3b6e380a01b
Ashley Ratcliffe <ar419@kent.ac.uk>
2025-01-22 19:12:12 +0000

    User Profile part 1

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("userProfileIcon").addEventListener("click", function() {
+  const dropdown = document.getElementById("userProfileDropdown");
+
+  if (dropdown.style.display == "none" || dropdown.style.display == "") {
+    dropdown.style.display = "block";
+  } else {
+    dropdown.style.display = "none";
+  }
+});
